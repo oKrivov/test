32fb86c5927e83cce86039cb46a6410442c914b2
oKrivov <krivov.exceedteam@gmail.com>
2021-07-03 19:06:07 +0300

    add fiter

--- ToDo_vanilla_JS/app.js
+++ ToDo_vanilla_JS/app.js
@@ -1,26 +1,45 @@
+
 'use strict'
 
 //Selectors
-let todoInput = document.querySelector('.input_todo');
-let todo = document.querySelector('.container_new_task');
-
+let todoInput = document.querySelector('.input_task');
+let todoList = document.querySelector('.container_new_task');
+const clear_completed = document.querySelector('.clear_completed');
+const filterOption = document.querySelector('.info-buttons');
 
 
 
 //Event Listeners
-todoInput.addEventListener('keydown', function(e) {
+todoInput.addEventListener('keydown', function (e) {
     if (e.keyCode === 13) {
+        // console.log(e);
         if (!todoInput.value) return;
-        let newTodo = {
-            todo: todoInput.value,
-            checked: false,
-            important: false,
-        }
-        addTdo();
+        addTdo(e);
     }
 });
+// todoList.addEventListener('click', toggleCheck);
+filterOption.addEventListener('click', filterTodo);
+clear_completed.addEventListener('click', clearCompleted);
+
 
 //FUNCTIONS
+function clearCompleted() {
+    console.log(todoList.children)
+    console.log(todoList.children)
+    for (let item of todoList.children){
+        if (item.classList.contains('completed')){
+            console.log(item);
+            item.remove();
+        }
+    }
+    // todoList.childNodes.forEach(function (item) {
+    //     if (item.classList.contains('completed')){
+    //         console.log(item);
+    //         item.remove();
+    //     }
+    // })
+}
+
 function addTdo(e) {
     // TODO DIV
     const todoDiv = document.createElement('div');
@@ -29,31 +48,83 @@ function addTdo(e) {
     // Create checkbox
     const label = document.createElement('label');
     label.classList.add('label');
+    const labelCheckbox = document.createElement("input");
+    labelCheckbox.classList.add('checkbox')
+    labelCheckbox.type = 'checkbox';
+    label.appendChild(labelCheckbox)
+    const fake_checkbox = document.createElement("span");
+    fake_checkbox.classList.add('fake-checkbox')
+    label.appendChild(fake_checkbox)
+
+    // label.innerHTML = `<input type="checkbox" class="checkbox" >
+	//                  <span class="fake-checkbox"></span>`;
+    labelCheckbox.addEventListener('change', toggleCheck)
     todoDiv.appendChild(label);
-    const checkBox = document.createElement('input');
-    checkBox.innerHTML = `<input type="checkbox" class="checkbox" >
-                          <span class="fake-checkbox"></span>`
-    label.appendChild(checkBox);
-    // const fakeCheckBox = document.createElement('span');
-    // fakeCheckBox.classList.add('fake-checkbox');
-    // label.appendChild(fakeCheckBox);
 
     // Create task
-    const task_text_container = document.createElement('div');
-    task_text_container.classList.add('task_container');
-    todoDiv.appendChild(task_text_container);
+    // const task_text_container = document.createElement('div');
+    // task_text_container.classList.add('task_container');
+    // todoDiv.appendChild(task_text_container);
 
     // Create taskText
     const taskText = document.createElement('span');
     taskText.innerText = todoInput.value;
     taskText.classList.add('label_text');
-    task_text_container.appendChild(taskText);
+    label.appendChild(taskText);
 
     // Create Close-btn
     const closeButton = document.createElement('a');
     closeButton.classList.add('my-btn-close');
-    task_text_container.appendChild(closeButton);
+    closeButton.addEventListener('click', deleteTodo);
+    todoDiv.appendChild(closeButton);
+
     // Append to todo
-    todo.appendChild(todoDiv);
+    todoList.appendChild(todoDiv);
+
+    // Clear todoInput
+    todoInput.value = '';
 
 }
+
+function toggleCheck(e) {
+    const item = e.target;
+    // Check mark
+    if (item.classList.contains('checkbox')) {
+        const todo = item.parentElement.parentElement;
+        // console.log(todo)
+        todo.classList.toggle('completed');
+    }
+
+}
+
+function deleteTodo(e) {
+    const item = e.target;
+    // Delete todo
+    if (item.classList.contains('my-btn-close')) {
+        const todo = item.parentElement;
+        todo.remove();
+
+    }
+}
+
+function filterTodo(e) {
+
+    const todos = todoList.children;
+
+    for (const todo of todos) {
+
+        switch (e.target.innerText.toLowerCase()) {
+            case 'all':
+                todo.style.display = 'flex';
+                break;
+            case 'completed':
+                todo.style.display = todo.classList.contains('completed') ? 'flex' : 'none';
+                break;
+            case 'active':
+                todo.style.display = !todo.classList.contains('completed') ? 'flex' : 'none';
+                break;
+        }
+    }
+}
+
+
