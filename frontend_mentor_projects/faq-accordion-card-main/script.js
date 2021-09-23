98703d8381268174555e93aa0445483fe5a11dc2
oKrivov <krivovoleg23@gmail.com>
2021-09-23 18:59:22 +0300

    finished web adaptive desing accordion-card

--- frontend_mentor_projects/faq-accordion-card-main/script.js
+++ frontend_mentor_projects/faq-accordion-card-main/script.js
@@ -1,13 +1,9 @@
 const questions = document.querySelectorAll('.question');
 
 questions.forEach(function(question) {
-	// console.log(question);
-	const btn = question.querySelector('.question-btn');
-	// const questionTitle = question.querySelector('.question-title');
+	const btn = question.querySelector('.question-title');
 
-	// console.log(questionTitle);
 	btn.addEventListener('click', function() {
-
 		questions.forEach(function(item) {
 			if (item !== question) {
 				item.classList.remove('show-text');
@@ -17,9 +13,9 @@ questions.forEach(function(question) {
 
 		question.classList.toggle('show-text');
 		question.classList.toggle('active');
-
-	})
+	});
 })
+
 
 
 
@@ -31,3 +27,97 @@ questions.forEach(function(question) {
 // 		question.classList.toggle('show-text');
 // 	});
 // })
+
+
+
+
+
+/*
+		<!-- single question -->
+							<div class="question">
+								<!--  question title -->
+								<div class="question-title">
+									<p>
+										What is the maximum file upload size?
+									</p>
+									<button class="question-btn">
+										<span class="down-arrow">
+											<img src="images/icon-arrow-down.svg" alt="">
+										</span>
+									</button>
+								</div>
+								<!-- question text -->
+								<div class="question-text">
+									<p>
+										You can invite up to 2 additional users on the Free plan. There is no limit on
+										team members for the Premium plan.
+									</p>
+								</div>
+							</div>
+							<!-- end single question -->
+							<!-- single question -->
+							<div class="question">
+								<!--  question title -->
+								<div class="question-title">
+									<p>
+										How do I reset my password?
+									</p>
+									<button class="question-btn">
+										<span class="down-arrow">
+											<img src="images/icon-arrow-down.svg" alt="">
+										</span>
+									</button>
+								</div>
+								<!-- question text -->
+								<div class="question-text">
+									<p>
+										You can invite up to 2 additional users on the Free plan. There is no limit on
+										team members for the Premium plan.
+									</p>
+								</div>
+							</div>
+							<!-- end single question -->
+							<!-- single question -->
+							<div class="question">
+								<!--  question title -->
+								<div class="question-title">
+									<p>
+										Can I cancel my subscription?
+									</p>
+									<button class="question-btn">
+										<span class="down-arrow">
+											<img src="images/icon-arrow-down.svg" alt="">
+										</span>
+									</button>
+								</div>
+								<!-- question text -->
+								<div class="question-text">
+									<p>
+										Yes! Send us a message and we’ll process your request no questions asked.
+									</p>
+								</div>
+							</div>
+							<!-- end single question -->
+							<!-- single question -->
+							<div class="question">
+								<!--  question title -->
+								<div class="question-title">
+									<p>
+										Do you provide additional support?
+									</p>
+									<button class="question-btn">
+										<span class="down-arrow">
+											<img src="images/icon-arrow-down.svg" alt="">
+										</span>
+									</button>
+								</div>
+								<!-- question text -->
+								<div class="question-text">
+									<p>
+										Chat and email support is available 24/7. Phone lines are open during normal business
+										hours.
+									</p>
+								</div>
+							</div>
+							<!-- end single question -->
+*/
